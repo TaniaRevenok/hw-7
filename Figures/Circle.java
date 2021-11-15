3114f8e09958d5a0496b1940dc83cab9c4b12b46
TaniaRevenok <89931570+TaniaRevenok@users.noreply.github.com>
2021-11-15 15:44:31 +0200

    Create Circle.java

---
+++ Figures/Circle.java
@@ -0,0 +1,18 @@
+import hw_7.Shape;
+
+public class Circle extends Shape {
+
+    private String name;
+
+    public Circle() {
+
+        this.name = "Circle";
+    }
+
+    @Override
+    public String getName() {
+
+        return name;
+    }
+
+}
