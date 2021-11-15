5c9587d87dc9cb921ef470c35babdb8c51d8036b
TaniaRevenok <89931570+TaniaRevenok@users.noreply.github.com>
2021-11-15 15:30:05 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Shape[] shapes = new Shape[5];
+        shapes[0] = new Circle();
+        shapes[1] = new Rectangle();
+        shapes[2] = new Diamond();
+        shapes[3] = new Triangle();
+        shapes[4] = new Quad();
+
+        for (Shape shape : shapes) {
+            new PrintFigure(shape).printName();
+        }
+
+    }
+
+}
---
+++ Shape.java
@@ -0,0 +1,9 @@
+public abstract class Shape implements Print {
+
+    @Override
+    public void printName() {
+        System.out.println(getName());
+    }
+
+    public abstract String getName();
+}
